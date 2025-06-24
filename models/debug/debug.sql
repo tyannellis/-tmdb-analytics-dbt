1220d34607c2b984a821b1405918390ba6776405
Tyann Ellis <tyannellis1@gmail.com>
2025-06-24 20:11:31 +0000

    removing table materialization and debug

--- models/debug/debug.sql
+++
@@ -1,6 +0,0 @@
--- models/debug/debug_env_check.sql
-SELECT
-  '{{ target.name }}' AS target_name,
-  '{{ target.database }}' AS database_name,
-  '{{ target.schema }}' AS schema_name,
-  CURRENT_TIMESTAMP AS time_of_run
